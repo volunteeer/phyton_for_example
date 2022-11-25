8d10cfc8448aa53c1a76e3fae29384c9aaac2d5a
Dmitry <volunteer@gmail.com>
2022-11-25 23:50:10 +0300

    typo

--- 008.py
+++ 008.py
@@ -1,3 +1,3 @@
 totalPrice = int(input("Let's coutn your bill!\n Enter total bill price!\n"))
 numberOfGuests = int(input("Enter the number of guests\n"))
-print("You nees to pay ", int(totalPrice / numberOfGuests))
+print("You need to pay ", int(totalPrice / numberOfGuests))
