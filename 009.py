d27f56dd01243d4b4cf51e877e1fe17f6f0fbea4
Dmitry <volunteer@gmail.com>
2022-11-26 00:21:45 +0300

    009-010

---
+++ 009.py
@@ -0,0 +1,5 @@
+days = int(input("Eneter the number of days you whant to devide on hours minutes and seconds\n"))
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print("In", days , "days there will be:\n", hours, "hours\n", minutes, "minutes\n", "and", seconds,"seconds")
