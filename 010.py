ebae1d052b2b723b6a33e51219f5d1d6fb3870d4
Dmitry <volunteer@gmail.com>
2022-11-26 14:08:39 +0300

    10-11

---
+++ 010.py
@@ -0,0 +1,3 @@
+kiloes = int(input("Lets convert Kilos to Pounds\nEnter the kiloes "))
+pounds = kiloes * 2,204
+print ("There will be", pounds,"in", kiloes,"kiloes" )
---
+++ 011.py
@@ -0,0 +1,6 @@
+from imaplib import Int2AP
+
+
+big = int(input("Enter a number bigger that 100 \n"))
+small = int(input("Enter number less than 10 \n"))
+print (big,"is",big//small,"times bigger than",small)
